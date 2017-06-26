ad47b008e8a4b2b7e14554321a5be2c337c3c550
Francisco Casas Barrientos <francisco.casas.13@sansano.usm.cl>
2017-06-26 17:29:25 -0400

    Some comments.

--- pg/state.c
+++ pg/state.c
@@ -1,17 +1,21 @@
 #include "state.h"
 
+// NOTE: Both pgpiece_hash and pgpiece_compare functions must change if pgpiece does!
+
 uint pgpiece_hash(const pgpiece *piece){
-    // This function must be modified if the definition of the pgpiece changes!
+    // Gets a hash of a given piece.
     return *((uint *) piece);
 }
 
 int pgpiece_compare(const void *p_a, const void *p_b){
+    // Compares two pieces using their numeric representation.
     uint hash_a = pgpiece_hash((const pgpiece*) p_a);
     uint hash_b = pgpiece_hash((const pgpiece*) p_b);
     return hash_a-hash_b;
 }
 
 uint pgstate_sort_and_hash(const pgstate *state){
+    // Sorts the pieces of a state and then calculates it's hash.
     qsort((void*)state->pieces,state->n_pieces,sizeof(pgpiece),pgpiece_compare);
     uint hash = 0;
     for(int k=0;k<state->n_pieces;k++){
