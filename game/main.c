c35f9384e28f0f69a8e376c91c06ecc5c8b3d6ae
Autopawn <franciscojacb@gmail.com>
2017-07-27 19:50:12 -0400

    Drawing pieces in the right position.

--- game/main.c
+++ game/main.c
@@ -6,8 +6,9 @@
 
 #define SCREEN_WIDTH 480
 #define SCREEN_HEIGHT 480
+#define BORDER 16
 
-#define TILE_SIZE 64
+#define MAX_TILE_SIZE 64
 
 #define N_SPRITES 1
 const char* sprite_dirs[] = {
@@ -52,7 +53,7 @@ void init_game() {
         sprites[i] = NULL;
         // Load image at specified path
         sprites[i] = IMG_Load(sprite_dirs[i]);
-        if(sprites[i] == NULL){
+        if(sprites[i] == NULL) {
             printf("Unable to load image %s! SDL_image Error: %s\n",
                 sprite_dirs[i], IMG_GetError());
             exit(1);
@@ -74,27 +75,35 @@ void close_game() {
 }
 
 void display_level(pglevel *level, pgstate *state) {
+    int tile_size_x = (SCREEN_WIDTH - BORDER * 2) / (level->max_x + 2) / 2;
+    int tile_size_y = (SCREEN_HEIGHT - BORDER * 2) / (level->max_y + 2) / 2;
+    int tile_size = (tile_size_x > tile_size_y) ? tile_size_y : tile_size_x;
+    tile_size *= 2;
+    tile_size = (tile_size > MAX_TILE_SIZE) ? MAX_TILE_SIZE : tile_size;
     // Draw background
     SDL_FillRect(screen_surface, NULL,
         SDL_MapRGB(screen_surface->format, 255, 255, 255));
     // Draw level
-    for(int y=0;y<=level->max_y;y++){
-        for(int x=0;x<=level->max_x;x++){
+    for(int y = 0; y <= level->max_y; y++) {
+        for(int x = 0; x <= level->max_x; x++) {
             // Draw tile
+            // ...
         }
     }
     // Draw pieces
-    for(int i=0;i<state->n_pieces;i++){
+    for(int i=0;i<state->n_pieces;i++) {
         // Decide sprite for the kind of the piece
         int spr_index = state->pieces[i].kind;
         if(spr_index<=N_SPRITES) spr_index = N_SPRITES-1;
         // Draw piece in the right place
         SDL_Rect dstrect;
-        dstrect.x = TILE_SIZE*state->pieces[i].p_x;
-        dstrect.y = TILE_SIZE*state->pieces[i].p_y;
-        dstrect.w = sprites[spr_index]->w;
-        dstrect.h = sprites[spr_index]->h;
-        SDL_BlitSurface(sprites[spr_index], NULL, screen_surface, &dstrect);
+        dstrect.x = tile_size/2*(2*state->pieces[i].p_x-level->max_x-1);
+        dstrect.x += SCREEN_WIDTH/2;
+        dstrect.y = tile_size/2*(2*state->pieces[i].p_y-level->max_y-1);
+        dstrect.y += SCREEN_HEIGHT/2;
+        dstrect.w = tile_size;
+        dstrect.h = tile_size;
+        SDL_BlitScaled(sprites[spr_index], NULL, screen_surface, &dstrect);
     }
     // Update display
     SDL_UpdateWindowSurface(window);
@@ -103,10 +112,10 @@ void display_level(pglevel *level, pgstate *state) {
 int play_level(pglevel *level, pgstate *state) {
     int n_moves = 0;
     pgstate_hash(state);
-    while(1){
+    while(1) {
         display_level(level,state);
         pgresult result;
-		while(1){
+		while(1) {
 			result = PUZZLE_RULE(level,state);
 			if(result.conclusion!=STEP) break;
 			*state = result.next.step;
@@ -116,30 +125,40 @@ int play_level(pglevel *level, pgstate *state) {
 			display_level(level,state);
 		}
         // React to a choice
-        if(result.conclusion==CHOICE){
-            if(result.n_choices<=0){
+        if(result.conclusion==CHOICE) {
+            if(result.n_choices<=0) {
                 printf("NO CHOICES!\n");
                 return -1;
             }else{
                 printf("CHOICE %d:\n",++n_moves);
-                for(int i=0;i<result.n_choices;i++){
+                for(int i=0;i<result.n_choices;i++) {
                     printf("%6d) %s\n",i,result.next.choices[i].description);
                 }
                 // Wait for user input
-                while(1){
-                    int answer;
-                    printf("Choice: ");
-                    scanf("%d",&answer);
-                    if(0<=answer && answer<result.n_choices){
-                        *state = result.next.choices[answer].resulting;
-                        pgstate_hash(state);
-                        break;
-                    }else{
-                        printf("Enter a valid number!\n");
+                while(1) {
+                    SDL_Event ev;
+                    while(SDL_PollEvent(&ev) != 0) {
+                        if(ev.type == SDL_QUIT) {
+                            // User requests quit
+                            return -3;
+                        }else if(ev.type == SDL_KEYDOWN) {
+                            // Handle key presses
+                            // ...
+                        }
                     }
+                    // int answer;
+                    // printf("Choice: ");
+                    // scanf("%d",&answer);
+                    // if(0<=answer && answer<result.n_choices) {
+                    //     *state = result.next.choices[answer].resulting;
+                    //     pgstate_hash(state);
+                    //     break;
+                    // }else{
+                    //     printf("Enter a valid number!\n");
+                    // }
                 }
             }
-        }else if(result.conclusion==WIN){
+        }else if(result.conclusion==WIN) {
             printf("WIN ON %d CHOICES!\n",n_moves);
             return n_moves;
         }else{
@@ -159,11 +178,8 @@ int main(int argc, char const *argv[]) {
     pgstate state;
     pgread_from_file("lvls/slide02.txt", &level, &state);
     play_level(&level, &state);
-    SDL_BlitSurface(sprites[0], NULL, screen_surface, NULL);
     // Update the surface
     SDL_UpdateWindowSurface(window);
-    // Wait two seconds
-    SDL_Delay(2000);
     // Close systems
     close_game();
     return 0;
